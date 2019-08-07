c291360674c47f5889ccb02e58cb576d23cd8927
justynchen <justynchen@tencent.com>
2019-08-07 14:37:26 +0800

    fix: lint

--- test/client_code.js
+++ test/client_code.js
@@ -1,12 +1,13 @@
 let sdk = require('../index')
 
+//eslint-disable-next-line no-unused-vars
 exports.main_handler = async (event, context, callback) => {
   let db1 = new sdk('TESTDB1')
   let db2 = new sdk('TESTDB2')
 
   db1.getConnection((err,connection)=>{
     if(!err){
-      connection.query('select * from test',(err,results,fields)=>{
+      connection.query('select * from test',(err,results)=>{
         if(!err){
           console.log(results)
         }else{
@@ -20,7 +21,7 @@ exports.main_handler = async (event, context, callback) => {
 
   db2.getConnection((err,connection)=>{
     if(!err){
-      connection.query('select * from coffee',(err,results,fields)=>{
+      connection.query('select * from coffee',(err,results)=>{
         if(!err){
           console.log(results)
         }else{
